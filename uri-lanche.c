ef2a3e08f540c28d6cb1c24b8b35d49bd64996da
João Pedro São Gregorio Silva <pedrosgregorio9@gmail.com>
2018-04-06 09:49:47 -0300

    Formatando e mudando nome de variáveis

--- uri-lanche.c
+++ uri-lanche.c
@@ -1,42 +1,39 @@
 #include <stdio.h>
 
 int main() {
-    int qtdade=0, codigo;
-    double total=0;
+  int quantidade = 0;
+  int codigo = 0;
+  double total = 0;
 
-        scanf("%d", &codigo);
-        scanf("%d", &qtdade);
+  scanf("%d", &codigo);
+  scanf("%d", &quantidade);
 
-        switch (codigo) {
-            case 1:
-                total = total + 4.00;
-                break;
+  switch (codigo) {
+  case 1:
+    total = total + 4.00;
+    break;
 
-            case 2:
-                total = total + 4.50;
-                break;
+  case 2:
+    total = total + 4.50;
+    break;
 
-            case 3:
-                total = total + 5.00;
-                break;
+  case 3:
+    total = total + 5.00;
+    break;
 
-            case 4:
-                total = total + 2.00;
-                break;
+  case 4:
+    total = total + 2.00;
+    break;
 
-            case 5:
-                total = total + 1.50;
-                break;
+  case 5:
+    total = total + 1.50;
+    break;
 
-            default:
-                break;
-        }
-        total = total*qtdade;
+  default:
+    break;
+  }
+  total = total * quantidade;
 
-
-
-
-      printf("Total: R$: %.2f", total);
-
-    return 0;
+  printf("Total: R$: %.2f", total);
+  return 0;
 }
